dda168b6871ff5a9b17874ece216e50589e88a5b
mamallik <gajji.mallikarjun@iamplus.com>
2018-02-27 16:20:52 +0530

    adding java prog

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+
+	public static void main(String args[]) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] arr = br.readLine().split("\\s"); 
+		for(String s: arr) System.out.print(s+" ");
+	} 
+
+}
