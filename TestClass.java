1eee29fabaacd0a107b49c0d45c49261505e74fb
Mallikarjun <mallikarjun.gajji@honestbee.com>
2018-08-31 22:27:59 +0530

    adding testclass.java

---
+++ TestClass.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.math.*;
+
+class TestClass {
+
+    public static void main(String args[] ) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] sa = br.readLine().split("\\s");
+        Scanner s = new Scanner(System.in);
+    }
+
+    static void p(String s){
+      System.out.print(s);
+    }
+
+    static void log(String s){
+      p(s);
+    }
+}
